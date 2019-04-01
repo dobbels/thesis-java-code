c9f75dc92151e7ae04ec0543382d58189c81b1c3
Instant Contiki <steven.dobbelaere@hotmail.com>
2019-04-01 19:00:39 +0200

    make empty security assocation class

--- src/hidra/HidraMessage.java
+++ src/hidra/HidraMessage.java
@@ -8,16 +8,8 @@ import java.util.Random;
  */
 public class HidraMessage {
 
-	
-	// Om dit allemaal aan te passen
-	// TODO kijk in paper of berichten echt deftig beschreven
-	// TODO anders: code van een char om aan te geven welk bericht van het protocol het is
-	// TODO maak alle velden aan die nodig zijn in bepaalde berichten en schrijf de beschrijving over van de paper
-	// Maak methoden als 'isAnAnsReqMessage' alleen op een as-needed basis. Je weet dat anders veel dingen onnodig gemaakt zullen zijn
-			// !! Want echt veel verspilde tijd om altijd voor te bereiden op dingen. Werk van PoC naar PoC!
-	// TODO (in constructor zonder argumenten: zet al alle gemeenschappelijke delen. -> obviously)
-	// Zie DHCP message als dat helpt
-	
-	
+	//TODO constructor met bvb alleen id en dan subklasses voor elk soort bericht? Of zelfs geen gemeenschappelijke velden?
+	// 		OF
+	//TODO gewoon static functies maken : constructAnsReq, constructAnsRep enz -> natuurlijk alleen van diegene die je in Subject en ACS nodig hebt! 
 
 }
---
+++ src/hidra/HidraSecurityAssociation.java
@@ -0,0 +1,8 @@
+package hidra;
+
+public class HidraSecurityAssociation {
+	// TODO Id/Code van een paar bits om aan te geven welk bericht van het protocol het is?
+	// TODO maak alle velden aan die bestaan in een security association en schrijf de beschrijving over van de paper
+	 
+		
+}
